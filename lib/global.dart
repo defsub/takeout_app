f5037278f9dc873dd1a9d5ef6df202fd4668886b
defsub <defsub@defsub.com>
2022-03-30 11:37:49 -0700

    v0.7.1

--- lib/global.dart
+++ lib/global.dart
@@ -23,7 +23,7 @@ import 'package:audio_service/audio_service.dart';
 import 'schema.dart';
 import 'artists.dart'; // TODO remove
 
-const appVersion = '0.7.0';
+const appVersion = '0.7.1';
 const appSource = 'https://github.com/defsub/takeout_app';
 const appHome = 'https://takeout.fm';
 
